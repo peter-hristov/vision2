6909a52e66894825145c8dcec4252a038580fbc8
famouscake <mm16pgh@leeds.ac.uk>
2017-03-21 11:42:24 +0000

    Almost working.

--- index.m
+++ index.m
@@ -1,64 +1,91 @@
-
+% Read images
 [foo Anames] = fileattrib( 'images/butterfly/image*');
 [foo Cnames] = fileattrib( 'images/chairs/image*');
 [foo Bnames] = fileattrib( 'images/laptop/image*');
 [foo Dnames] = fileattrib( 'images/motorbikes/image*');
 
-images = cat(2,Anames,Bnames,Cnames,Dnames);
-%images = Anames;
 
-patchNum = 100;
-patchSize = 100;
-imageNum = 10;
+% Parameters
+patchNum = 500;
+patchSize = 5;
+imageNum = 200;
 codebookSize = 10;
 
-F = [];
+images = cat(2, Anames, Bnames, Cnames, Dnames);
+
+% Select 5 of each at random to be test images
+testIndices = [randperm(50, 5); randperm(50, 5) + 50; randperm(50, 5) + 100; randperm(50, 5) + 150];
+
+F = zeros(patchNum * imageNum, patchSize * patchSize);
 
 % Read and filter images
-%for i = 1:size(images, 2)
 for i = 1:imageNum
 
     image = im2double(imread(images(i).Name));
 
+    % If image was read in an RGB format
     if size(image, 3) > 1
         image = rgb2gray(image);
     end
 
+    % Filter image
     image = lcn(image);
 
-
+    % Get feature coordinates (top left corner)
     patchesX = randi([1, size(image, 1) - patchSize], patchNum, 1);
     patchesY = randi([1, size(image, 2) - patchSize], patchNum, 1);
     P = [patchesX patchesY];
 
-    images(i).Features = [];
+    images(i).Features = zeros(patchNum, patchSize^2);
 
-    for j = 1:size(P, 1)
+    % Extract descriptors
+    for j = 1:patchNum
         feature = image(P(j, 1):(P(j, 1) + patchSize - 1), P(j, 2):(P(j, 2) + patchSize - 1));
-        images(i).Features = [images(i).Features feature(:)];
-
-        F = [F feature(:)];
-
-        %figure
-        %imshow(feature)
+        images(i).Features(j, :) = feature(:);
+        %imwrite(feature, ['./features/element-' num2str(i) '-' num2str(j) '.bmp'], 'bmp');
     end
 
 end
 
-% Get Code Words by clustering
-[idx C] = kmeans(F', codebookSize);  % cluster HS
+% Get Codebook by clustering all features in the training data
+[idx Codebook] = kmeans(vertcat(images(~ismember(1:end, testIndices(:))).Features), codebookSize);
 
-% Print the codebook
-for i = 1:size(C, 1)
-    x = C(i, :)';
-    x = reshape(x, patchSize, patchSize);
+% Write the codebook
+for i = 1:codebookSize
+    codeWord = reshape(Codebook(i, :)', patchSize, patchSize);
+    imwrite(codeWord, ['./codebook/element-' num2str(i) '.bmp'], 'bmp');
+end
 
-    %figure
-    %imshow(x)
-
-    imwrite(x,['./codebook/element-' num2str(i)], 'bmp');
-
+% Make histograms for all images
+for i = 1:imageNum
+    A = nearestneighbour(images(i).Features, Codebook);
+    images(i).Histogram = histcounts(A, codebookSize , 'Normalization', 'probability');
 end
 
 
-clear all
+%f = F(1:5,:);
+%size(f);
+%size(C);
+
+%F(1,:)
+%A = nearestneighbour(f, C);
+
+%size(C(A, :))
+
+%A
+%H = histcounts(A, 10, 'Normalization', 'probability')
+
+%a = reshape(C(i, :)', patchSize, patchSize);
+%b = reshape(C(i, :)', patchSize, patchSize);
+
+%figure
+%imshow(f)
+
+%figure
+%imshow(f)
+
+%clear all
+
+
+
+
